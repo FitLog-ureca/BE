b4a373a20dae656d1ee1d1c48fa8f174f11ce1a5
joonhyong <dbshwns0528@naver.com>
2025-12-21 18:51:43 +0900

    FEAT: 달력의 해당 날짜가 isDone이 true일 때만 체크 표시

--- src/main/java/com/ureca/fitlog/todos/dto/response/TodoDailySummaryDTO.java
+++ src/main/java/com/ureca/fitlog/todos/dto/response/TodoDailySummaryDTO.java
@@ -1,5 +1,7 @@
 package com.ureca.fitlog.todos.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,4 +17,12 @@ public class TodoDailySummaryDTO {
     private LocalDate date;
     private int totalSets;
     private int completedSets;
+
+    @JsonProperty("isDone")
+    private boolean isDone;
+
+    @JsonIgnore
+    public boolean isDone() {
+        return isDone;
+    }
 }
